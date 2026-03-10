93463027b23576ee471c0365c66f1161caf8ae10
leslieannumali031788 <umalileslieann10@gmail.com>
2026-03-10 20:42:53 +0800

    Finished coding: a three line code for a program that prints the bigger number.

---
+++ printing_bigger_number.py
@@ -0,0 +1,3 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+print("The bigger number among the inputted numbers is: ", number1 if number1 > number2 else number2)
